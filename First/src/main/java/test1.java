969c81f51948ac1d366bf988fb21db096d2e9add
guxiaoayong <guxiaoayong888@gmail.com>
2016-09-13 22:46:58 +0800

    first commit

---
+++ First/src/main/java/test1.java
@@ -0,0 +1,14 @@
+package main.java;
+
+/**
+ * Created by xiaoyong on 9/12/16.
+ */
+public class test1 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+
+    }
+}
---
+++ Test/src/main/java/com/chinaredstar/test/Test1.java
@@ -0,0 +1,9 @@
+package com.chinaredstar.test;
+
+/**
+ * Created by xiaoyong on 9/12/16.
+ */
+public class Test1 {
+
+
+}
